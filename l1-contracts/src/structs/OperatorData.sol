23f73c0fbcb670d04989b7ffb8015b4a6d1a31e0
JasonVranek <jasonvranek@gmail.com>
2024-08-22 21:44:09 -0700

    feat: Add OperatorCommitment struct and update OperatorData struct

--- l1-contracts/src/structs/OperatorData.sol
+++ l1-contracts/src/structs/OperatorData.sol
@@ -6,15 +6,24 @@ pragma solidity >=0.8.0 <0.9.0;
  * @notice Struct to store information about an operator in the UniFi AVS system.
  * @dev This struct is used to keep track of important operator details.
  */
+struct OperatorCommitment {
+    /// @notice The delegate key for the operator.
+    bytes delegateKey;
+    /// @notice Bitmap of chain IDs the operator is committed to.
+    uint256 chainIDBitMap;
+}
+
 struct OperatorData {
     /// @notice The number of validators associated with this operator.
     uint128 validatorCount;
-    /// @notice The delegate key for the operator.
-    bytes delegateKey;
+    /// @notice The current commitment of the operator.
+    OperatorCommitment commitment;
     /// @notice The block number when the operator started the deregistration process.
     uint256 startOperatorDeregisterBlock;
-    bytes pendingDelegateKey;
-    uint256 delegateKeyValidAfter;
+    /// @notice The pending commitment of the operator.
+    OperatorCommitment pendingCommitment;
+    /// @notice The block number after which the pending commitment becomes valid.
+    uint256 commitmentValidAfter;
 }
 
 /**
