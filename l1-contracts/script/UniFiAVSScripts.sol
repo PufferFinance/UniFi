bed42a75cd2f6dd3accb87ac5ea3be7a27a1eb15
0xwalid <walid.wah@gmail.com>
2024-08-26 09:37:42 -0700

    adds adding valdiators from a json file

--- l1-contracts/script/UniFiAVSScripts.sol
+++ l1-contracts/script/UniFiAVSScripts.sol
@@ -3,14 +3,21 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Script.sol";
 import { UniFiAVSManager } from "../src/UniFiAVSManager.sol";
+import "../src/structs/ValidatorData.sol";
+import "../src/structs/OperatorData.sol";
 import { ISignatureUtils } from "eigenlayer/interfaces/ISignatureUtils.sol";
 import "../test/mocks/MockEigenPodManager.sol";
 import "../test/mocks/MockDelegationManager.sol";
 import "../test/mocks/MockAVSDirectory.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { IEigenPod } from "eigenlayer/interfaces/IEigenPod.sol";
 
 // to run the script: forge script script/UniFiAVSScripts.sol:UniFiAVSScripts --sig "createEigenPod(address)" "0xabcdefg..."
 
 contract UniFiAVSScripts is Script {
+    using Strings for uint256;
+
     MockDelegationManager mockDelegationManager;
     MockEigenPodManager mockEigenPodManager;
     UniFiAVSManager uniFiAVSManager;
@@ -57,7 +64,7 @@ contract UniFiAVSScripts is Script {
     // Action 4: Register an Operator with UniFiAVSManager and set initial commitment (the caller of this script should be the operator)
     function registerOperatorToUniFiAVS(
         ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature,
-        UniFiAVSManager.OperatorCommitment memory initialCommitment
+        OperatorCommitment memory initialCommitment
     ) public {
         vm.startBroadcast();
         uniFiAVSManager.registerOperator(operatorSignature);
@@ -73,7 +80,7 @@ contract UniFiAVSScripts is Script {
     }
 
     // Action 6: Set the Operator's Commitment
-    function setOperatorCommitment(UniFiAVSManager.OperatorCommitment memory newCommitment) public {
+    function setOperatorCommitment(OperatorCommitment memory newCommitment) public {
         vm.startBroadcast();
         uniFiAVSManager.setOperatorCommitment(newCommitment);
         vm.stopBroadcast();
@@ -104,6 +111,7 @@ contract UniFiAVSScripts is Script {
     function setupPodAndRegisterValidators(
         address podOwner,
         address operator,
+        OperatorCommitment memory initialCommitment,
         bytes32[] memory pubkeyHashes,
         MockEigenPod.ValidatorInfo[] memory validators,
         ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature
@@ -113,17 +121,51 @@ contract UniFiAVSScripts is Script {
         // Step 1: Create a Mock Pod
         createEigenPod(podOwner);
 
-        // Step 2: Add Validators to MockEigenPod
-        addValidatorsToEigenPod(podOwner, pubkeyHashes, validators);
-
-        // Step 3: Delegate from PodOwner to Operator
+        // Step 2: Delegate from PodOwner to Operator
         delegateFromPodOwner(podOwner, operator);
 
-        // Step 4: Register the Operator
-        registerOperatorToUniFiAVS(operatorSignature);
+        // Step 3: Register the Operator
+        registerOperatorToUniFiAVS(operatorSignature, initialCommitment);
 
+        // Step 4: Add Validators to MockEigenPod
+        addValidatorsToEigenPod(podOwner, pubkeyHashes, validators);
 
         // Step 5: Register Validators with UniFiAVSManager
         registerValidatorsToUniFiAVS(podOwner, pubkeyHashes);
+    }
+
+    function addValidatorsFromJsonFile(string memory filePath, address podOwner) public {
+        // Read the JSON file as a string
+        string memory jsonData = vm.readFile(filePath);
+
+        // Parse the JSON string into a dynamic array of objects
+        string[] memory validatorsData = vm.parseJsonStringArray(jsonData, ".data");
+
+        bytes32[] memory pubkeyHashes = new bytes32[](validatorsData.length);
+        IEigenPod.ValidatorInfo[] memory validators = new IEigenPod.ValidatorInfo[](validatorsData.length);
+
+        // Iterate over the array and extract the required fields
+        for (uint256 i = 0; i < validatorsData.length; i++) {
+            // Extract index and pubkey from each object
+            string memory validatorJson = validatorsData[i];
+
+            uint256 index = vm.parseJsonUint(validatorJson, ".index");
+
+            bytes memory pubkey = vm.parseJsonBytes(validatorJson, ".validator.pubkey");
+
+            pubkeyHashes[i] = keccak256(pubkey);
+            validators[i] = IEigenPod.ValidatorInfo({
+                validatorIndex: uint64(index),
+                restakedBalanceGwei: 0,
+                mostRecentBalanceUpdateTimestamp: 0,
+                status: IEigenPod.VALIDATOR_STATUS.ACTIVE
+            });
+
+            mockEigenPodManager.setValidator(podOwner, pubkeyHashes[i], validators[i]);
+
+            console.log("Added validator with index:", index);
+        }
+
+        uniFiAVSManager.registerValidators(podOwner, pubkeyHashes);
     }
 }
