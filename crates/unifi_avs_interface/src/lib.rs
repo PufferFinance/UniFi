77ad5f645219711098078b83cd53c098fb4619a7
JasonVranek <jasonvranek@gmail.com>
2024-08-26 14:37:47 -0700

    feat: Add unifi_avs_interface library

---
+++ crates/unifi_avs_interface/src/lib.rs
@@ -0,0 +1,27 @@
+use alloy::{sol, sol_types};
+
+sol!(
+    #[sol(rpc)]
+    UniFiAVSManager,
+    "../../l1-contracts/out/UniFiAVSManager.sol/UniFiAVSManager.json"
+);
+
+// impl OperatorCommitment {
+//     const PACKED_ENCODED_SIZE: Option<usize> = None;
+// }
+
+// impl OperatorData {
+//     const PACKED_ENCODED_SIZE: Option<usize> = None;
+// }
+
+// impl ValidatorData {
+//     const PACKED_ENCODED_SIZE: Option<usize> = None;
+// }
+
+// impl OperatorDataExtended {
+//     const PACKED_ENCODED_SIZE: Option<usize> = None;
+// }
+
+// impl ValidatorDataExtended {
+//     const PACKED_ENCODED_SIZE: Option<usize> = None;
+// }
